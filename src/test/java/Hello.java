a5ed88af7f085646b20305000b3d74c6c02cf7a3
wuchaoyu <wuchaoyu@corp.netease.com>
2019-05-14 14:25:50 +0800

    减少comment

---
+++ src/test/java/Hello.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Hello {
+    public void say() {
+        ArrayBlockingQueue<String>data;
+        ConcurrentLinkedQueue<String>data2;
+    }
+
+    public void done() {
+
+    }
+}
--- src/test/java/ResourceTest.java
+++ src/test/java/ResourceTest.java
@@ -1,42 +1,41 @@
 import org.junit.Test;
+import sun.misc.ProxyGenerator;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.nio.Buffer;
 import java.util.Enumeration;
 
 public class ResourceTest {
     @Test
     public void test() throws IOException {
         Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources("cn/edu/bupt/bean");
-        while(urls.hasMoreElements()){
+        while (urls.hasMoreElements()) {
 
-           URL url = urls.nextElement();
-           if(url.getProtocol().equals("file"))
-           {
+            URL url = urls.nextElement();
+            if (url.getProtocol().equals("file")) {
 
-               File[] files = new File(url.getPath()).listFiles(new FileFilter() {
-                   public boolean accept(File file) {
+                File[] files = new File(url.getPath()).listFiles(new FileFilter() {
+                    public boolean accept(File file) {
 
-                        return (file.isDirectory()||file.getName().endsWith(".class"));
+                        return (file.isDirectory() || file.getName().endsWith(".class"));
 
-                   }
-               });
+                    }
+                });
 
-            for(File file:files){
+                for (File file : files) {
 
-                handleFodler(file);
+                    handleFodler(file);
+                }
+
+
+            } else if (url.getProtocol().equals("jar")) {
+
+
             }
-
-
-           }else if(url.getProtocol().equals("jar")){
-
-
-
-           }
         }
     }
+
     @Test
     public void handleJar() throws IOException {
 
@@ -45,27 +44,45 @@ public class ResourceTest {
     }
 
 
-    public void handleFodler(File file){
+    public void handleFodler(File file) {
 
-        if(file.isDirectory())
-        {
+        if (file.isDirectory()) {
 
             File[] subFiles = file.listFiles(new FileFilter() {
                 public boolean accept(File pathname) {
-                    return (pathname.isDirectory()||pathname.getName().endsWith(".class"));
+                    return (pathname.isDirectory() || pathname.getName().endsWith(".class"));
                 }
             });
 
-            for(File subFile:subFiles){
+            for (File subFile : subFiles) {
                 handleFodler(subFile);
             }
 
-        }else{
+        } else {
 
             System.out.println(file.getName());
 
         }
     }
+
+    @Test
+    public void test2() throws IOException {
+
+        byte[] data = ProxyGenerator.generateProxyClass("name", new Class[]{Hello.class});
+
+        FileOutputStream fos = new FileOutputStream("D:/name.class");
+        fos.write(data);
+        fos.close();
+    }
+
+    @Test
+    public void testNumberOfJVM() {
+
+        Buffer buffer;
+        System.out.println(System.getProperty("sun.arch.data.model")); // 检测虚拟机位数
+
+    }
+
 
 
 }
